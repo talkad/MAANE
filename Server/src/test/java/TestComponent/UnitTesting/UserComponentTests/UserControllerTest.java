54af6f124a3da6afdf762f5e62e7ac24f18e613b
shaked6 <shaked6@post.bgu.ac.il>
2021-11-28 20:26:45 +0200

    added 1 more test and fixed another

--- Server/src/test/java/TestComponent/UnitTesting/UserComponentTests/UserControllerTest.java
+++ Server/src/test/java/TestComponent/UnitTesting/UserComponentTests/UserControllerTest.java
@@ -26,6 +26,17 @@ public class UserControllerTest {
         String newGuestName = userController.logout(adminName).getResult();
         userController.login(newGuestName, "sup1", "sup1");
         Assert.assertTrue(userController.getConnectedUsers().containsKey("sup1"));
-        Assert.assertTrue(userController.getRegisteredUsers().containsKey(adminName));
+    }
+
+    @Test
+    public void assigningInstructorBySupervisorSuccess(){
+        UserController userController = UserController.getInstance();
+        String guestName = userController.addGuest().getResult();
+        String adminName = userController.login(guestName, "shaked", "cohen").getResult();
+        userController.registerUser(adminName, "sup1", "sup1", UserStateEnum.SUPERVISOR);
+        String newGuestName = userController.logout(adminName).getResult();
+        userController.login(newGuestName, "sup1", "sup1");
+        userController.registerUser("sup1", "ins1", "ins1", UserStateEnum.INSTRUCTOR);
+        Assert.assertTrue(userController.getRegisteredUsers().containsKey("ins1"));
     }
 }
