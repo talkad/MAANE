422483b072982ebecad4a4ab8128d168b4a5bbb6
aviadtzemah <aviad896@gmail.com>
2021-11-28 20:07:10 +0200

    tests skeleton

---
+++ Server/src/main/java/GuidingBasketsManagement/tests/SearchEngineTest.java
@@ -0,0 +1,37 @@
+package GuidingBasketsManagement.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchEngineTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void addBasket() {
+    }
+
+    @Test
+    public void removeBasket() {
+    }
+
+    @Test
+    public void setBasketTitle() {
+    }
+
+    @Test
+    public void setBasketDescription() {
+    }
+
+    @Test
+    public void addRemoveLabel() {
+    }
+}
