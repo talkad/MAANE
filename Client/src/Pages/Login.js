6904dd5f668b08823a199acbd58ce351fe2b00e2
aviadtzemah <aviad896@gmail.com>
2021-12-11 17:49:00 +0200

    messing around

--- Client/src/Pages/Login.js
+++ Client/src/Pages/Login.js
@@ -5,7 +5,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import {FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import {FormControl, IconButton, InputAdornment, InputLabel, Input,} from "@mui/material";
 
 export default function Login(){
     const [values, setValues] = useState({
@@ -31,6 +31,7 @@ export default function Login(){
 
     return (
         <Box className="Login">
+            <h1>מענ"ה</h1>
             <Box
                 className="Login-form"
                 component="form"
@@ -39,11 +40,22 @@ export default function Login(){
                 }}
                 noValidate
                 autoComplete="off">
-                <TextField dir="rtl" id="username" label="שם משתמש" variant="standard" />
-                <FormControl sx={{ m: 1, width: '25ch'}} variant="standard">
-                    <InputLabel htmlFor="outlined-adornment-password">סיסמה</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
+
+                <InputLabel dir='rtl' sx={{textAlign: 'right', display: 'block',
+                    float: 'right', paddingBottom: '0'}}><b>שם משתמש:</b></InputLabel>
+                <FormControl dir='rtl' sx={{ m: 1, width: '25ch', }} variant="standard">
+                    <Input
+                        type='text'
+                        value={values.username}
+                        onChange={handleChange('username')}
+                    />
+                </FormControl>
+
+
+                <InputLabel dir='rtl' sx={{textAlign: 'right', display: 'block',
+                    float: 'right', paddingBottom: '0'}}><b>סיסמה:</b></InputLabel>
+                <FormControl dir='rtl' sx={{ m: 1, width: '25ch', }} variant="standard">
+                    <Input
                         type={values.showPassword ? 'text' : 'password'}
                         value={values.password}
                         onChange={handleChange('password')}
@@ -53,18 +65,36 @@ export default function Login(){
                                     aria-label="toggle password visibility"
                                     onClick={handleClickShowPassword}
                                     onMouseDown={handleMouseDownPassword}
-                                    edge="end"
                                 >
                                     {values.showPassword ? <VisibilityOff /> : <Visibility />}
                                 </IconButton>
                             </InputAdornment>
                         }
-                        label="password"
                     />
                 </FormControl>
+                {/*<FormControl variant="standard">*/}
+                {/*    <InputLabel>סיסמה</InputLabel>*/}
+                {/*    <OutlinedInput>*/}
+                {/*        type={values.showPassword ? 'text' : 'password'}*/}
+                {/*        value={values.password}*/}
+                {/*        onChange={handleChange('password')}*/}
+                {/*        endAdornment={*/}
+                {/*        <InputAdornment position='end'>*/}
+                {/*            <IconButton*/}
+                {/*                onClick={handleClickShowPassword}*/}
+                {/*                onMouseDown={handleMouseDownPassword}*/}
+                {/*            >{values.showPassword ? <VisibilityOff/> : <Visibility/>}*/}
+                {/*            </IconButton>*/}
+                {/*        </InputAdornment>*/}
+                {/*    }*/}
+                {/*    label='password'*/}
+                {/*    </OutlinedInput>*/}
+                {/*</FormControl>*/}
+
+                <Button variant="outlined">כניסה</Button>
             </Box>
             <Box>
-                <Button variant="outlined">כניסה</Button>
+
             </Box>
         </Box>
 
