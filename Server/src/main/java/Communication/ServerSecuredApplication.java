f2ef50febc3a88966544518c285b7b72308c39b3
Tal Kadosh <talkad@post.bgu.ac.il>
2022-03-15 15:03:55 +0200

    redirection from http to https

--- Server/src/main/java/Communication/ServerSecuredApplication.java
+++ Server/src/main/java/Communication/ServerSecuredApplication.java
@@ -1,9 +1,14 @@
 package Communication;
 
+//import org.apache.catalina.Context;
+//import org.apache.catalina.connector.Connector;
+//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+//import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+//import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 // (exclude = { SecurityAutoConfiguration.class })
@@ -11,10 +16,32 @@ public class ServerSecuredApplication {
 
     public static void main(String[] args){
         SpringApplication.run(ServerSecuredApplication.class, args);
-
-//        SpringApplication application = new SpringApplication(ServerSecuredApplication.class);
-//        application.setAdditionalProfiles("ssl");
-//        application.run(args);
     }
+
+//    @Bean
+//    public EmbeddedServletContainerFactory createServletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcatContainer = new TomcatEmbeddedServletContainerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection securityCollection = new SecurityCollection();
+//                securityCollection.addPattern("/*");
+//                constraint.addCollection(securityCollection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcatContainer.addAdditionalTomcatConnectors(redirectConnectorHttp());
+//        return tomcatContainer;
+//    }
+//
+//    private Connector redirectConnectorHttp() {
+//        Connector redirectConnector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        redirectConnector.setScheme("http");
+//        redirectConnector.setPort(8080);
+//        redirectConnector.setSecure(false);
+//        redirectConnector.setRedirectPort(8443);
+//        return redirectConnector;
+//    }
 
 }
