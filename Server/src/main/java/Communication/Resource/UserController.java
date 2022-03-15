a1a97f3d4c41c29cc099a279ad2fd8cc598c7f4e
Tal Kadosh <talkad@post.bgu.ac.il>
2022-03-15 13:16:21 +0200

    code cleanup

--- Server/src/main/java/Communication/Resource/UserController.java
+++ Server/src/main/java/Communication/Resource/UserController.java
@@ -24,41 +24,41 @@ public class UserController {
         return "success";
     }
 
-//    @GetMapping("/startup")
-//    public ResponseEntity<Response<String>> startup(){
-//        return ResponseEntity.ok()
-//                .body(service.addGuest());
-//    }
-//
-//    @RequestMapping(value = "/login", method = RequestMethod.POST)
-//    public ResponseEntity<Response<Pair<String, UserStateEnum>>> login(@RequestBody Map<String, Object> body){
-//        return ResponseEntity.ok()
-//                .body(service.login((String)body.get("currUser"), (String)body.get("userToLogin"), (String)body.get("password")));
-//    }
-//
-//    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-//    public ResponseEntity<Response<String>> logout(@RequestBody Map<String, Object>  body){
-//        return ResponseEntity.ok()
-//                .body(service.logout((String)body.get("name")));
-//    }
-//
-//    @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
-//    public ResponseEntity<Response<User>> registerUser(@RequestBody UserDTO user) {
-//        return ResponseEntity.ok()
-//                .body(service.registerUser(user));
-//    }
-//
-//    @RequestMapping(value = "/removeUser", method = RequestMethod.POST)
-//    public ResponseEntity<Response<Boolean>> removeUser(@RequestBody Map<String, Object>  body){
-//        return ResponseEntity.ok()
-//                .body(service.removeUser((String)body.get("currUser"), (String)body.get("userToRemove")));
-//    }
-//
-//    @GetMapping("/viewWorkPlan/username={username}")
-//    public ResponseEntity<Response<WorkPlanDTO>> viewWorkPlan(@PathVariable("username") String username){
-//        return ResponseEntity.ok()
-//                .body(service.viewWorkPlan(username));
-//    }
+    @GetMapping("/startup")
+    public ResponseEntity<Response<String>> startup(){
+        return ResponseEntity.ok()
+                .body(service.addGuest());
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<Response<Pair<String, UserStateEnum>>> login(@RequestBody Map<String, Object> body){
+        return ResponseEntity.ok()
+                .body(service.login((String)body.get("currUser"), (String)body.get("userToLogin"), (String)body.get("password")));
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ResponseEntity<Response<String>> logout(@RequestBody Map<String, Object>  body){
+        return ResponseEntity.ok()
+                .body(service.logout((String)body.get("name")));
+    }
+
+    @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
+    public ResponseEntity<Response<User>> registerUser(@RequestBody UserDTO user) {
+        return ResponseEntity.ok()
+                .body(service.registerUser(user));
+    }
+
+    @RequestMapping(value = "/removeUser", method = RequestMethod.POST)
+    public ResponseEntity<Response<Boolean>> removeUser(@RequestBody Map<String, Object>  body){
+        return ResponseEntity.ok()
+                .body(service.removeUser((String)body.get("currUser"), (String)body.get("userToRemove")));
+    }
+
+    @GetMapping("/viewWorkPlan/username={username}")
+    public ResponseEntity<Response<WorkPlanDTO>> viewWorkPlan(@PathVariable("username") String username){
+        return ResponseEntity.ok()
+                .body(service.viewWorkPlan(username));
+    }
 
 //    @RequestMapping(value = "/removeUser", method = RequestMethod.POST)
 //    public ResponseEntity<Response<Boolean>> generateSchedule(@RequestBody Map<String, Object>  body){
