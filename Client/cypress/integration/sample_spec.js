0c9d7edd07c1bd381ead9c4fdc9b71cdf597b0c5
aviadtzemah <aviad896@gmail.com>
2022-04-17 16:15:08 +0300

    first dummy test and somechanges

---
+++ Client/cypress/integration/sample_spec.js
@@ -0,0 +1,13 @@
+
+
+describe('My First Test', () =>{
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+})
+
+describe('My First Test', () =>{
+    it('Visits the Kitchen Sink', () => {
+        cy.visit('https://example.cypress.io')
+    })
+})
