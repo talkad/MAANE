058b35698dd2c26f1d36dcf3a70d47da166f5dde
aviadtzemah <aviad896@gmail.com>
2022-04-21 17:12:42 +0300

    modifications to the change password feature in the user management page

--- Client/cypress/integration/end-to-end/users_management_tests.js
+++ Client/cypress/integration/end-to-end/users_management_tests.js
@@ -1,9 +1,22 @@
 
+let selected_user_as_supervisor = {
+    username: "vivenna",
+}
+
 describe('User management test as a supervisor', () => {
     beforeEach(() => {
         //TODO: format the db and log in as a supervisor
         cy.visit('/user/home')
+
+        cy.get(`#user_collapse_button_${selected_user_as_supervisor.username}`).click()
     })
+
+    it('Successfully change password to user', () => {
+        // opening the change password dialog
+        cy.get(`#change_password_${selected_user_as_supervisor.username}`).click()
+
+    })
+
 })
 
 describe('User management test as a system manager', () => {
