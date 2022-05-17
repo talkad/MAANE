fde2dba81cf929ed8c50bbd8bdaed10a068972c6
talkad <talkad@post.bgu.ac.il>
2022-05-17 11:32:45 +0300

    note for future @talkad - AsposeJavaAPI conversion from html to docx isn't working

--- maane/maane/src/main/java/Domain/MonthlyReport/MonthlyReportGenerator.java
+++ maane/maane/src/main/java/Domain/MonthlyReport/MonthlyReportGenerator.java
@@ -1,36 +1,19 @@
 package Domain.MonthlyReport;
 
-import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import com.aspose.words.Document;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 
-import java.io.File;
+import java.io.IOException;
 
 public class MonthlyReportGenerator {
 
-    public static void main(String[] args) throws Docx4JException {
-        WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
-        MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
-        mainDocumentPart.addStyledParagraphOfText("Title", "Hello World!");
-        mainDocumentPart.addParagraphOfText("Welcome To Baeldung");
+    public static void main(String[] args) throws Exception {
 
-//        int writableWidthTwips = wordPackage.getDocumentModel()
-//                .getSections().get(0).getPageDimensions().getWritableWidthTwips();
-//        int columnNumber = 3;
-//        Tbl tbl = TblFactory.createTable(3, 3, writableWidthTwips/columnNumber);
-//        List<Object> rows = tbl.getContent();
-//        for (Object row : rows) {
-//            Tr tr = (Tr) row;
-//            List<Object> cells = tr.getContent();
-//            for(Object cell : cells) {
-//                Tc td = (Tc) cell;
-//                td.getContent().add(p);
-//            }
-//        }
-
-        File exportFile = new File("welcome.docx");
-        wordPackage.save(exportFile);
-
+//        MSWordWriterService service = new MSWordWriterService();
+//
+//        service.createDoc("maane\\src\\main\\resources\\my_file.docx");
+//
 
     }
 
