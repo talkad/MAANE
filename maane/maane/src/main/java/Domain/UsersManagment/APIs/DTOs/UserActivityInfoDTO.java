1520b0bee0a3433daecafe71edf5124146f91ae7
talkad <talkad@post.bgu.ac.il>
2022-05-17 23:17:48 +0300

    updated DTOs

--- maane/maane/src/main/java/Domain/UsersManagment/APIs/DTOs/UserActivityInfoDTO.java
+++ maane/maane/src/main/java/Domain/UsersManagment/APIs/DTOs/UserActivityInfoDTO.java
@@ -7,7 +7,11 @@ import java.time.LocalDateTime;
 @Data
 public class UserActivityInfoDTO {
 
-    private LocalDateTime date;
+    private LocalDateTime activityStart;
+    private LocalDateTime activityEnd;
     private int totalHours;
     private String schoolName;
+    private String userCity;
+    private String schoolCity;
+
 }
--- maane/maane/src/main/java/Domain/UsersManagment/APIs/DTOs/UserInfoDTO.java
+++ maane/maane/src/main/java/Domain/UsersManagment/APIs/DTOs/UserInfoDTO.java
@@ -2,16 +2,13 @@ package Domain.UsersManagment.APIs.DTOs;
 
 import lombok.Data;
 
-import java.util.List;
 
 @Data
 public class UserInfoDTO {
 
     private String lastName;
     private String firstName;
-    private int numInstructionDays;
-
-    // index of the working days, when sunday-0, monday-1 ...
-    private List<Integer> workingDays;
+    private String city;
+    private int workingDay;
 
 }
