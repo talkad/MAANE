d2f29d2a45636005eef4b1fcd82aff99513d38c2
talkad <talkad@post.bgu.ac.il>
2022-05-18 17:18:02 +0300

    activated https

--- maane/maane/src/main/java/Communication/MaaneApplication.java
+++ maane/maane/src/main/java/Communication/MaaneApplication.java
@@ -144,30 +144,5 @@ public class MaaneApplication {
 		};
 	}
 
-//    @Bean
-//    public EmbeddedServletContainerFactory createServletContainer() {
-//        TomcatEmbeddedServletContainerFactory tomcatContainer = new TomcatEmbeddedServletContainerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint constraint = new SecurityConstraint();
-//                constraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection securityCollection = new SecurityCollection();
-//                securityCollection.addPattern("/*");
-//                constraint.addCollection(securityCollection);
-//                context.addConstraint(constraint);
-//            }
-//        };
-//        tomcatContainer.addAdditionalTomcatConnectors(redirectConnectorHttp());
-//        return tomcatContainer;
-//    }
-//
-//    private Connector redirectConnectorHttp() {
-//        Connector redirectConnector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        redirectConnector.setScheme("http");
-//        redirectConnector.setPort(8080);
-//        redirectConnector.setSecure(false);
-//        redirectConnector.setRedirectPort(8443);
-//        return redirectConnector;
-//    }
 
 }
